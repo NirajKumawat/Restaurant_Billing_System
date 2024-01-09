6bffbda6bde461a0412fa018c15316ef1ecc0ae1
Niraj Kumawat <145189348+NirajKumawat@users.noreply.github.com>
2024-01-09 22:59:46 +0530

    Update C_Project.c

--- C_Project.c
+++ C_Project.c
@@ -2,15 +2,20 @@
 #include<string.h>
 #include<stdlib.h>
 
-struct items{
-    char item[20];
-    float price;
-    int qty;
-}
+// struct items{
+//     char item[20];
+//     float price;
+//     int qty;
+// }
 
-struct orders{
-    char customer[50];
-    char date[50];
-    int num_of_items;
-    struct items itm[50];
-}
+// struct orders{
+//     char customer[50];
+//     char date[50];
+//     int num_of_items;
+//     struct items itm[50];
+// }
+
+// fgets(ord.customer,50,stdin);
+// ord.customer[strlen(ord.customer)-1]=0;
+// strcpy(ord.date,_date_);
+// ord.itm[i].itme[strlen(ord.itm[i].item)-1]=0;
