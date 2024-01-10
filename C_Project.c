79bfc569e6b749b46ffb9c42bcf8970b4471cfca
Niraj Kumawat <145189348+NirajKumawat@users.noreply.github.com>
2024-01-10 23:13:52 +0530

    Update C_Project.c
    added a bill header function

--- C_Project.c
+++ C_Project.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<dos.h>
 
+void generateBillHeader(char name[50],char date[30]){
+    printf("\n\n\t\tXYZ Restaurant");
+    printf("G.T Road, Indore, Madhya Pradesh");
+    printf("\nPHONE NO : XXXXXX7461, XXXXXX9850");
+    printf("--------------------------------------------------");
+    printf("Date : ");
+    printf("Bill No. : ");
+    printf("Table No. : ");
+}
+
+int main(){
+
+}
 // struct items{
 //     char item[20];
 //     float price;
