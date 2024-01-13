60c9d6b962bc9c6685419c4a84d51f34354fb2bc
Niraj Kumawat <145189348+NirajKumawat@users.noreply.github.com>
2024-01-13 23:41:30 +0530

    completed generate invoice function

--- C_Project.c
+++ C_Project.c
@@ -3,116 +3,107 @@
 #include<stdlib.h>
 #include<time.h>
 
-void generateInvoice();
-void billHeader();
-void billBody(int num_of_items);
-void billfooter();
+void billHeader(){//char name[50],char date[30]
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    int TBnum;
+    printf("\nEnter Table No. : ");
+    scanf("%d",&TBnum);
+    printf("\n\n\t\tXYZ Restaurant");
+    printf("\n\tG.T Road, Indore, Madhya Pradesh");
+    printf("\n\tPHONE NO : XXXXXX7461, XXXXXX9850");
+    printf("\n__________________________________________________");
+    printf("\n Bill No. : ");
+    printf("\t\tDate : %02d/%02d/%02d %02d:%02d",tm.tm_mday,tm.tm_mon,tm.tm_year,tm.tm_hour,tm.tm_min);
+    printf("\n Table No. : %d",TBnum);
+    printf("\n__________________________________________________");
+    printf("\n\tItem\t\t\tQty.\tPrice");
+    printf("\n__________________________________________________");
+    
+}
+
+void billBody(char item[30],float qty,float price){
+    if(strlen(item)<7){
+    printf("\n %s\t\t\t\t%.2f\t%.2f",item,qty,price);
+    }
+    else if(strlen(item)<15){
+    printf("\n %s\t\t\t%.2f\t%.2f",item,qty,price);
+    }
+    else if(strlen(item)<23){
+    printf("\n %s\t\t%.2f\t%.2f",item,qty,price);
+    }
+    else if(strlen(item)<31){
+    printf("\n %s\t%.2f\t%.2f",item,qty,price);
+    }
+}
+
+void billFooter(float sub_total){
+    float tax,total;
+    tax=sub_total*0.09;  
+    total=sub_total+(2*tax);
+    printf("\n__________________________________________________");
+    printf("\n Sub Total \t\t\t\t%.2f",sub_total);
+    printf("\n CGST@9 on %.2f \t\t\t%.2f",sub_total,tax);
+    printf("\n SGST@9 on %.2f \t\t\t%.2f",sub_total,tax);
+    printf("\n Amount incl of all taxes \t\t%.2f\n",total);
+}
 
 struct Item{
     char itm[30];
-    int qty;
+    float qty;
     float price;
 };
 
 int main(){
     int choice;
+    int num_of_items;
+    struct Item item[30];
+    float sub_total=0,total_incl_taxes=0;
     printf("\n=================||XYZ RESTAURANT||===============\n");
-    printf("\nSelect Your Operation :");
-    printf("\n1.Generate Invoice ");
-    printf("\n2.Search Invoice ");
-    printf("\n3.Show all Invoice ");
-    printf("\n4.Exit \n");
 
     do{
+        printf("\n\nSelect Your Operation :");
+        printf("\n1.Generate Invoice ");
+        printf("\n2.Search Invoice ");
+        printf("\n3.Show all Invoice ");
+        printf("\n4.Exit \n");
         printf("\nEnter Your Choice : ");
         scanf("%d",&choice);
         switch(choice){
-            case(1):generateInvoice();
+            case(1):
+                printf("\nEnter the number of Items : ");
+                scanf("%d",&num_of_items);
+
+                for(int i=1;i<=num_of_items;i++){
+                    fgetc(stdin);
+                    printf("\n\nEnter the name of Item %d : ",i);
+                    fgets(item[i].itm,30,stdin);
+                    item[i].itm[strlen(item[i].itm)-1] = 0;
+
+                    printf("Enter the quantity of Item : ");
+                    scanf("%f",&item[i].qty);
+
+                    printf("Enter the price of Item : ");
+                    scanf("%f",&item[i].price);
+
+                    sub_total+=(item[i].qty*item[i].price);
+                }
+
+                billHeader();
+                for(int i=1;i<=num_of_items;i++){
+                    billBody(item[i].itm,item[i].qty,item[i].price);
+                }
+                billFooter(sub_total);
+
             break;
-            case(2):
+            case(2)://search invoice
             break;
-            case(3):
+            case(3)://show all invoice
             break;
-            case(4):
-            printf("\n\n\t\tThanks for Coming. :)\n\n");
+            case(4):printf("\n\n\t\tThanks for Coming. :)\n\n");
             break;
             default:printf("you entered wrong choice!");
         }
     }while(choice!=4);
 }
 
-void generateInvoice(){
-    int num_of_items;
-    struct Item item[30];
-    float sub_total=0,total_incl_taxes=0;
-    printf("\nEnter the number of Items : ");
-    scanf("%d",&num_of_items);
-
-    for(int i=1;i<=num_of_items;i++){
-        fgetc(stdin);
-        printf("\n\nEnter the name of Item %d : ",i);
-        fgets(item[i].itm,30,stdin);
-        item[i].itm[strlen(item[i].itm)-1] = 0;
-
-        printf("Enter the quantity of Item : ");
-        scanf("%d",&item[i].qty);
-
-        printf("Enter the price of Item : ");
-        scanf("%f",&item[i].price);
-
-        sub_total+=(item[i].qty*item[i].price);
-
-    }
-    printf("%.2f",sub_total);
-    billHeader();
-    for(int i=1;i<=num_of_items;i++){
-        printf("\n%s\t\t\t%d\t\t%.2f",item[i].itm,item[i].qty,item[i].price);
-    }
-}
-
-void billHeader(){//char name[50],char date[30]
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    int TBnum;
-    printf("Enter Table No. : ");
-    scanf("%d",&TBnum);
-    printf("\n\n\t\tXYZ Restaurant");
-    printf("\n\tG.T Road, Indore, Madhya Pradesh");
-    printf("\n\tPHONE NO : XXXXXX7461, XXXXXX9850");
-    printf("\n__________________________________________________");
-    printf("\nBill No. : ");
-    printf("\t\tDate : %02d/%02d/%02d %02d:%02d",tm.tm_mday,tm.tm_mon,tm.tm_year,tm.tm_hour,tm.tm_min);
-    printf("\nTable No. : %d",TBnum);
-    printf("\n__________________________________________________");
-    printf("\n\tItem\t\tQty.\t\tPrice");
-    printf("\n__________________________________________________");
-    
-}
-
-void billBody(int num_of_items){
-        // printf("\ntandoori roti\t\t4\t\t60.00");
-    // printf("bill body");
-
-}
-
-void billFooter(){
-    printf("\n__________________________________________________");
-    printf("FOOTER  ");
-}
-// struct items{
-//     char item[20];
-//     float price;
-//     int qty;
-// }
-
-// struct orders{
-//     char customer[50];
-//     char date[50];
-//     int num_of_items;
-//     struct items itm[50];
-// }
-
-// fgets(ord.customer,50,stdin);
-// ord.customer[strlen(ord.customer)-1]=0;//this is to stop conflict of scanf and fgets
-// strcpy(ord.date,_date_); 
-// ord.itm[i].itme[strlen(ord.itm[i].item)-1]=0;
